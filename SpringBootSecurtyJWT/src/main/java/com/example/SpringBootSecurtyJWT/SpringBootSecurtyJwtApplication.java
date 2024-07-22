a43415011ae4b7c9687b542bb5ddd5daf5de61b8
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-07-22 20:06:27 +0530

    create Project

---
+++ SpringBootSecurtyJWT/src/main/java/com/example/SpringBootSecurtyJWT/SpringBootSecurtyJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSecurtyJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurtyJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurtyJwtApplication.class, args);
+	}
+
+}
---
+++ SpringBootSecurtyJWT/src/test/java/com/example/SpringBootSecurtyJWT/SpringBootSecurtyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSecurtyJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurtyJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
